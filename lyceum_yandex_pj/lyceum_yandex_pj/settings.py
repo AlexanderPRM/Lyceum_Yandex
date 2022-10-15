cad92fef880bef7b63baf865bbc21083105a71c8
Александр Пермяков <nathan920@yandex.ru>
2022-10-16 00:14:29 +0300

    Ready to Rewiev

--- lyceum_yandex_pj/lyceum_yandex_pj/settings.py
+++ lyceum_yandex_pj/lyceum_yandex_pj/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Application definition
 
