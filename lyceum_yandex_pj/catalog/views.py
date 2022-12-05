f2b4c8c67da04ca7eea81818d02115ecc70c7249
Andrulik <94921596+Prettywoman13@users.noreply.github.com>
2022-12-05 22:04:38 +0200

    Update views.py

--- lyceum_yandex_pj/catalog/views.py
+++ lyceum_yandex_pj/catalog/views.py
@@ -11,8 +11,8 @@ from rating.forms import RateForm
 
 class ItemList(ListView):
     model = Item
-    template_name = "pages/catalog/main.html"
-    context_object_name = "items"
+    template_name = 'pages/catalog/main.html'
+    context_object_name = 'items'
 
     def get_queryset(self):
         return Item.objects.published_item()
@@ -20,15 +20,15 @@ class ItemList(ListView):
 
 class ItemDetail(DetailView, UpdateView):
     model = Item
-    template_name = "pages/catalog/item_detail.html"
+    template_name = 'pages/catalog/item_detail.html'
     form_class = RateForm
 
     def get_initial(self, **kwargs):
         '''Закидываем в форму выбранную оценку, если такая есть'''
         initial = super(ItemDetail, self).get_initial(**kwargs)
         try:
-            initial["rate"] = ItemRating.objects.get(
-                item__pk=self.kwargs["pk"], user__pk=self.request.user.id
+            initial['rate'] = ItemRating.objects.get(
+                item__pk=self.kwargs['pk'], user__pk=self.request.user.id
             ).rate
         except ObjectDoesNotExist:
             pass
@@ -36,17 +36,17 @@ class ItemDetail(DetailView, UpdateView):
 
     def get_context_data(self, **kwargs):
         context = super(ItemDetail, self).get_context_data(**kwargs)
-        context["avg_rate"] = ItemRating.objects.item_avg_rate(
-            item_pk=kwargs["object"].id
+        context['avg_rate'] = ItemRating.objects.item_avg_rate(
+            item_pk=kwargs['object'].id
         )
         if self.request.user.is_authenticated:
-            context["user_rate"] = ItemRating.objects.get_user_rate(
-                user_pk=self.request.user.id, item_pk=kwargs["object"].id
+            context['user_rate'] = ItemRating.objects.get_user_rate(
+                user_pk=self.request.user.id, item_pk=kwargs['object'].id
             )
         else:
-            context["user_rate"] = None
+            context['user_rate'] = None
         return context
 
     def form_valid(self, form):
-        form.save(self.request.user, self.kwargs["pk"])
-        return redirect("catalog:item_detail", self.kwargs["pk"])
+        form.save(self.request.user, self.kwargs['pk'])
+        return redirect('catalog:item_detail', self.kwargs['pk'])
