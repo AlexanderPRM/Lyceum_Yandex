058b3a69b3ff13270ebb4027e6ac96ec83d1fae6
Александр Пермяков <nathan920@yandex.ru>
2022-12-07 14:40:13 +0300

    test fix

--- lyceum_yandex_pj/lyceum_yandex_pj/tests.py
+++ lyceum_yandex_pj/lyceum_yandex_pj/tests.py
@@ -1,20 +1,33 @@
-#  @classmethod
-#     def setUpClass(cls) -> None:
-#         super().setUpClass()
-#         cls.user1 = User.objects.create(
-#             email="mail2@mail.com",
-#             password="12344dre",
-#             birthday=datetime.today(),
-#         )
-#          def test_birthday_on_page(self):
-#         response = Client().get(reverse("about:main"))
-#         self.assertIn("birthdays", response.context)
+from django.test import TestCase
+from django.test.client import Client
+from django.urls import reverse
 
-#     def test_user_in_birthday_table(self):
-#         response = Client().get(reverse("about:main"))
-#         self.assertIn(self.user1.email,
-#                       response.context["birthdays"][0]["email"])
+# from unittest.mock import patch, Mock
 
-#     def tearDown(self):
-#         User.objects.all().delete()
-#         super().tearDown()
+from datetime import datetime
+from users.models import User
+
+
+class ContextProcessorTest(TestCase):
+    def setUpTestData(cls) -> None:
+        super().setUpTestData()
+        cls.user = User.objects.create(
+            email="mail2@mail.com",
+            password="12344dre",
+            birthday=datetime.today(),
+        )
+
+    def test_birthday_on_page(self):
+        response = Client().get(reverse("about:main"))
+        self.assertIn("birthdays", response.context)
+
+    def test_user_in_birthday_table(self):
+        response = Client().get(reverse("about:main"))
+        self.assertIsNotNone(response.context["birthdays"])
+        self.assertIn(response.context["birthdays"][0], "email")
+        self.assertIn(self.user1.email,
+                      response.context["birthdays"][0]["email"])
+
+    def tearDown(self):
+        User.objects.all().delete()
+        super().tearDown()
