343e34318f941d3484064d402629d1d15ad340d8
Prettywoman13 <AveSvecha@yandex.ru>
2022-12-07 16:20:37 +0200

    пофиксили тесты

--- lyceum_yandex_pj/lyceum_yandex_pj/tests.py
+++ lyceum_yandex_pj/lyceum_yandex_pj/tests.py
@@ -1,32 +1,55 @@
 from django.test import TestCase
 from django.test.client import Client
 from django.urls import reverse
+from parameterized import parameterized
 
 # from unittest.mock import patch, Mock
 
 from datetime import datetime
 from users.models import User
 
+URLS_LIST = [
+    ("about:main"),
+    ("catalog:item_list"),
+    ("feedback:feedback"),
+    ("homepage:main"),
+    ("users:register"),
+]
+
 
 class ContextProcessorTest(TestCase):
-    def setUpTestData(cls) -> None:
+    @classmethod
+    def setUpTestData(cls):
         super().setUpTestData()
-        cls.user = User.objects.create(
+        user = User.objects.create(
             email="mail2@mail.com",
             password="12344dre",
             birthday=datetime.today(),
+            first_name="Андрей",
         )
+        cls.user_email = user.email
+        cls.user_first_name = user.first_name
 
-    def test_birthday_on_page(self):
-        response = Client().get(reverse("about:main"))
+    @parameterized.expand(URLS_LIST)
+    def test_birthday_on_page(self, reverse_url):
+        response = Client().get(reverse(reverse_url))
         self.assertIn("birthdays", response.context)
 
-    def test_user_in_birthday_table(self):
-        response = Client().get(reverse("about:main"))
-        self.assertIsNotNone(response.context["birthdays"])
-        self.assertIn(response.context["birthdays"][0], "email")
-        self.assertIn(self.user1.email,
-                      response.context["birthdays"][0]["email"])
+    @parameterized.expand(URLS_LIST)
+    def test_birthday_length(self, reverse_url):
+        response = Client().get(reverse(reverse_url))
+        self.assertEqual(1, len(response.context["birthdays"]))
+
+    @parameterized.expand(URLS_LIST)
+    def test_user_in_birthdays_table(self, reverse_url):
+        response = Client().get(reverse(reverse_url))
+        self.assertEqual(
+            self.user_email, response.context["birthdays"][0]["email"]
+        )
+        self.assertEqual(
+            self.user_first_name,
+            response.context["birthdays"][0]["first_name"],
+        )
 
     def tearDown(self):
         User.objects.all().delete()
